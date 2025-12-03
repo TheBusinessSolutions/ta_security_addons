3d2d3edd60fb7351ffb35880d27a1617a88ff0fe
alihassanmostafa <alihassan.mostafa@gmail.com>
2025-12-03 20:56:24 +0200

    restore original code

--- sttl_journal_access_rights/models/account_journal.py
+++ sttl_journal_access_rights/models/account_journal.py
@@ -4,10 +4,4 @@ from odoo import models, fields
 class AccountJournal(models.Model):
     _inherit = "account.journal"
 
-    # Many2many field to store users explicitly allowed to use this journal.
     journal_users = fields.Many2many('res.users', string="Journal Users")
-
-# class AccountJournal(models.Model):
-#     _inherit = "account.journal"
-
-#     journal_users = fields.Many2many('res.users', string="Journal Users")
