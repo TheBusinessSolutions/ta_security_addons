1afadffb11508ffbb72e5f3caffa04297cbeac66
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-02-09 15:31:19 +0200

    .

--- ohrms_loan/models/hr_payslip.py
+++ ohrms_loan/models/hr_payslip.py
@@ -1,11 +1,33 @@
-from odoo import models, _
+# -*- coding: utf-8 -*-
+#############################################################################
+#    A part of Open HRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2023-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import models
 
 
 class HrPayslip(models.Model):
     """ Extends the 'hr.payslip' model to include
     additional functionality related to employee loans."""
     _inherit = 'hr.payslip'
-    
+
     def get_inputs(self, contract_ids, date_from, date_to):
         """Compute additional inputs for the employee payslip,
         considering active loans.
@@ -14,33 +36,37 @@ class HrPayslip(models.Model):
         :param date_to: End date of the payslip.
         :return: List of dictionaries representing additional inputs for
         the payslip."""
-        res = super(HrPayslip, self).get_inputs(contract_ids, date_from, date_to)
-        
+        res = super(HrPayslip, self).get_inputs(contract_ids, date_from,
+                                                date_to)
         employee_id = self.env['hr.contract'].browse(
-            contract_ids[0].id).employee_id if contract_ids else self.employee_id
-        
-        loans = self.env['hr.loan'].search([
-            ('employee_id', '=', employee_id.id),
-            ('state', '=', 'approve')
-        ])
-        
-        for loan in loans:
+            contract_ids[0].id).employee_id if contract_ids \
+            else self.employee_id
+        loan_id = self.env['hr.loan'].search(
+            [('employee_id', '=', employee_id.id), ('state', '=', 'approve')])
+        for loan in loan_id:
             for loan_line in loan.loan_lines:
-                if (date_from <= loan_line.date <= date_to and not loan_line.paid):
+                if (date_from <= loan_line.date <= date_to and
+                        not loan_line.paid):
                     for result in res:
                         if result.get('code') == 'LO':
                             result['amount'] = loan_line.amount
                             result['loan_line_id'] = loan_line.id
-        
         return res
 
+    # def action_payslip_done(self):
+    #     """ Compute the loan amount and remaining amount while confirming
+    #         the payslip"""
+    #     for line in self.input_line_ids:
+    #         if line.loan_line_id:
+    #             line.loan_line_id.paid = True
+    #             line.loan_line_id.loan_id._compute_total_amount()
+    #     return super(HrPayslip, self).action_payslip_done()
     def action_payslip_done(self):
-        """ Mark loan installments as paid 
-        The loan receivable reduction happens automatically via salary rule accounting """
+        """ Mark loan installment as paid when payslip is confirmed """
         for payslip in self:
             for line in payslip.input_line_ids:
                 if line.code == 'LO' and line.amount != 0:
-                    # Link loan line if missing
+                    # Find and link the loan line if missing
                     if not line.loan_line_id:
                         loan_line = self.env['hr.loan.line'].search([
                             ('employee_id', '=', payslip.employee_id.id),
@@ -51,21 +77,9 @@ class HrPayslip(models.Model):
                         if loan_line:
                             line.loan_line_id = loan_line.id
                     
-                    # Mark as paid
+                    # Mark as paid - this will auto-trigger loan balance recalculation
                     if line.loan_line_id:
-                        line.loan_line_id.write({
-                            'paid': True,
-                            'payslip_id': payslip.id
-                        })
+                        line.loan_line_id.paid = True
+                        # No need to manually call _compute_total_amount() anymore!
                         
-                        # Post notification on loan
-                        loan = line.loan_line_id.loan_id
-                        loan.message_post(
-                            body=_('Installment paid: %s %s via payslip %s') % (
-                                loan.currency_id.symbol,
-                                '{:,.2f}'.format(abs(line.amount)),
-                                payslip.number
-                            )
-                        )
-        
         return super(HrPayslip, self).action_payslip_done()
