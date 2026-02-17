72999d46ff23fa93d4f5b18369e6209147e3f3ad
alihassanmostafa <alihassan.mostafa@gmail.com>
2026-02-17 16:50:49 +0200

    .

--- hr_salary_advance/hr_salary_advance/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_salary_advance
-from . import hr_salary_advance_ledger
-from . import hr_payslip
--- hr_salary_advance/hr_salary_advance/wizard/__init__.py
+++
@@ -1,2 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import hr_advance_payment_wizard
